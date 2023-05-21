a4cf61287f45836f2a92696fdb8a716c561fd03d
Mahesh <124800760+MaheshVashisth@users.noreply.github.com>
2023-05-21 08:50:28 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { ChatEngine } from 'react-chat-engine';
+
+import ChatFeed from './components/ChatFeed';
+import LoginForm from './components/LoginForm';
+import './App.css';
+
+
+
+const App = () => {
+  if (!localStorage.getItem('username')) return <LoginForm />;
+  
+
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="2a53931a-dc3e-48e0-bcb0-bc3c7cf71f10"
+      userName={localStorage.getItem('username')}
+      userSecret={localStorage.getItem('password')}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+      onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
+    />
+  );
+};
+
+// infinite scroll, logout, more customizations...
+
+export default App;
